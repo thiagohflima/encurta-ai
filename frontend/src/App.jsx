6c83ee7c555a67eaaa5771805f38214ffceb36ac
Thiago Lima <thiagohflima@gmail.com>
2022-05-29 12:11:34 -0300

    Login cleaning after logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Background } from './components/Background';
 import { Login } from './components/Login';
 import { Table } from './components/Table';
@@ -6,6 +6,10 @@ import { Table } from './components/Table';
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [userId, setUserId] = useState(0);
+
+  useEffect(() => {
+    !loggedIn && setUserId(0);
+  }, [loggedIn]);
 
   return (
     <>
@@ -16,7 +20,10 @@ export default function App() {
       </div>
 
       {!loggedIn && <Login setLoggedIn={setLoggedIn} setUserId={setUserId} />}
-      {loggedIn && <Table setLoggedIn={setLoggedIn} />}
+      {loggedIn && <Table setLoggedIn={setLoggedIn} userId={userId} />}
+
+      <div>UserId: {userId}</div>
+      <div>loggedIn: {loggedIn ? 'True' : 'False'}</div>
 
       <div className='footer'>
         <span>Developed by:</span> Thiago H F Lima
